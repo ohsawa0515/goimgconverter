edc6730095d4d90089bf41e01d741b8dab9faac6
Shuichi Ohsawa <ohsawa0515@gmail.com>
2018-04-15 23:09:24 +0900

    processed the directory and recursively.

--- main.go
+++ main.go
@@ -2,31 +2,71 @@ package main
 
 import (
 	"image"
-	_ "image/jpeg"
+	"image/jpeg"
 	"image/png"
+	"io"
 	"log"
 	"os"
+	"path/filepath"
+
+	"github.com/pkg/errors"
 )
 
+func ImageEncode(r io.Reader, w io.Writer, ext string) error {
+	m, _, err := image.Decode(r)
+	if err != nil {
+		return err
+	}
+
+	switch ext {
+	case ".jpg", ".jpeg":
+		if err := png.Encode(w, m); err != nil {
+			return err
+		}
+	case ".png":
+		opts := &jpeg.Options{
+			Quality: 100,
+		}
+		if err := jpeg.Encode(w, m, opts); err != nil {
+			return err
+		}
+	default:
+		return errors.New("specify jpeg or png for the image format.")
+	}
+
+	if err := png.Encode(w, m); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func main() {
-	reader, err := os.Open("./ohsawa.jpeg")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer reader.Close()
+	root := "./test"
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+		r, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer r.Close()
 
-	m, _, err := image.Decode(reader)
-	if err != nil {
-		log.Fatal(err)
-	}
+		out := path[:len(path)-len(filepath.Ext(path))] + ".png"
+		w, err := os.OpenFile(out, os.O_CREATE|os.O_WRONLY, info.Mode())
+		if err != nil {
+			return err
+		}
+		defer w.Close()
 
-	writer, err := os.Create("./ohsawa.png")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer writer.Close()
+		if err := ImageEncode(r, w, filepath.Ext(path)); err != nil {
+			return err
+		}
 
-	if err := png.Encode(writer, m); err != nil {
+		return nil
+	})
+	if err != nil {
 		log.Fatal(err)
 	}
 
